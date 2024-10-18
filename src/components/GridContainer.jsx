5e00f8f39c5c78a5a37a3864ed3e8e32e8666d45
Mriganka Paul <ironmaniscool99@gmail.com>
2024-10-18 15:50:19 +0530

    Implemented landing page

--- src/components/GridContainer.jsx
+++ src/components/GridContainer.jsx
@@ -1,7 +1,7 @@
 function GridConatainer() {
     return (
         // <div className="h-[612.6px] grid grid-cols-15 grid-rows-7 gap-[15px] max-lg:grid-cols-10">
-        <div className="min-h-[612.6px] grid grid-cols-15 grid-rows-7 gap-[15px] max-lg:grid-cols-10 max-lg:h-auto">
+        <div className="min-h-[612.6px] grid grid-cols-15 grid-rows-7 gap-[15px] max-lg:grid-cols-10 max-lg:h-auto ">
             <div className=" light-shade rounded-[15px] row-span-4 col-span-6 max-lg:col-span-6 max-lg:300px"> </div>
             <div className=" dark-shade rounded-[15px] row-span-4 col-span-4 max-lg:col-span-4"> </div>
             <div className=" light-shade rounded-[15px] row-span-6 col-span-5 max-lg:col-span-full"> </div>
