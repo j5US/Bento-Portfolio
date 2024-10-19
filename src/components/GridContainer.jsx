c2ecdf5d841462553051106b68249955917b0abf
Mriganka Paul <ironmaniscool99@gmail.com>
2024-10-19 18:21:21 +0530

    Added Profile-image section

--- src/components/GridContainer.jsx
+++ src/components/GridContainer.jsx
@@ -23,7 +23,7 @@ function GridConatainer() {
             {/* <div className="light-shade rounded-[15px] row-span-1 col-span-5 max-lg:col-span-full max-lg:h-[80px]">Content 6</div> */}
 
             <Intro className="max-lg:col-span-6 max-lg:min-h-auto max-md:col-span-full"/>
-            <ProfileImage className="max-lg:col-span-4 max-lg:min-h-[350px] max-md:col-span-full"/>
+            <ProfileImage className="max-lg:col-span-4 max-lg:min-h-[400px] max-md:col-span-full"/>
 
             <About className="hidden max-lg:block max-lg:col-span-5 max-lg:h-auto max-md:col-span-full"/>
             <Contact className="hidden max-lg:block max-md:hidden max-lg:col-span-5 max-lg:h-[350px]"/>
--- src/sections/ProfileImage.jsx
+++ src/sections/ProfileImage.jsx
@@ -1,7 +1,8 @@
+// import profile from "../images/profile2.jpg"
 function ProfileImage({className}) {
     return (
-        <div className={`dark-shade rounded-[15px] row-span-4 col-span-4 ${className}`} >
-            Profile Image
+        <div className={`flex overflow-hidden dark-shade rounded-[15px] row-span-4 col-span-4 ${className}`} >
+            <div className="profile-image size-full"></div>
         </div>
     );
 }
