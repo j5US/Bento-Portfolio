aa66e2363e4afbe27000ec54a312cec53ebc2eeb
Mriganka Paul <ironmaniscool99@gmail.com>
2024-10-19 17:39:08 +0530

    Fixed Responsive Design for about section

--- src/sections/About.jsx
+++ src/sections/About.jsx
@@ -10,8 +10,8 @@ function About({ className }) {
 
                 <p
                     className="break-words about-text max-w-[30ch] pb-2 poppins-regular leading-[123%]
-                    max-lg:w-[25ch] max-lg:text-[1.2rem] max-lg:leading-[120%]
-                    max-md:w-[25ch] max-md:text-[1.2rem] max-md:leading-[110%]
+                    max-lg:w-[25ch] max-lg:text-[1.3rem] max-lg:leading-[120%]
+                    max-md:w-[24ch] max-md:text-[1.1rem] max-md:leading-[135%]
                 ">
                     Mriganka Paul is an aspiring full stack developer based in Chandigarh. He
                     combines technology and creativity to build engaging web
