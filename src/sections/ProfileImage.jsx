516151ee97253639a8bcab8df38117f36d0b3fa8
Mriganka Paul <ironmaniscool99@gmail.com>
2024-10-21 03:03:50 +0530

    test: responsive design

--- src/sections/ProfileImage.jsx
+++ src/sections/ProfileImage.jsx
@@ -2,7 +2,7 @@
 function ProfileImage({className}) {
     return (
         <div className={`overflow-hidden dark-shade rounded-[15px] row-span-4 col-span-4 ${className}`} >
-            <div className="profile-image size-full max-lg:aspect-[3/4] max-lg:aspect-[4/5]"></div>
+            <div className="profile-image size-full max-lg:aspect-[3/4] max-md:aspect-[4/5]"></div>
         </div>
     );
 }
