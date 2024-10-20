32eb41bcdc6fb98dc919a7f0c81422a79bcad48e
Mriganka Paul <ironmaniscool99@gmail.com>
2024-10-20 20:42:29 +0530

    added hambuger icon to navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,24 +1,28 @@
+import { CgMenu } from "react-icons/cg";
 function Navbar() {
     return (
         <div className="w-full h-[80px] light-shade rounded-[15px] mb-[15px]">
-            {/* max-lg:gap-4 max-md:flex-col  */}
             <nav
                 className="flex size-full items-center justify-between poppins-light
                 max-md:justify-center
             ">
-                <h1 className="text-2xl max-w-[13ch] ml-[20px] max-md:text-[22px]">
+                <h1 className="text-2xl max-w-[13ch] ml-[20px] max-md:ml-[0px] max-md:text-[23px]">
                     <i>Mriganka</i>
-                    <strong className="poppins-medium mx-1.5">Paul</strong>
+                    <strong className="poppins-medium mx-1.5 max-md:mr-[0px]">Paul</strong>
                 </h1>
 
                 <div
                     className="poppins-regular flex flex-row gap-[44px] mr-[20px]
-                    max-md:hidden
+                    max-md:hidden 
                 ">
                     <button>Projects</button>
                     <button>About</button>
                     <button>Contact</button>
                 </div>
+
+                <button className="absolute right-8 hidden text-2xl max-md:block ">
+                    <CgMenu />
+                </button>
             </nav>
 
         </div>
