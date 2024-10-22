97c897cb709650177abb1f32bf77f469e27a6449
Mriganka Paul <ironmaniscool99@gmail.com>
2024-10-23 03:20:19 +0530

    minor design fix

--- src/sections/Socials.jsx
+++ src/sections/Socials.jsx
@@ -2,7 +2,7 @@ function Social({ className }) {
     return (
         <div className={`light-shade rounded-[15px] row-span-1 col-span-5 ${className}`} >
             <div
-                className="flex size-full items-center justify-evenly gap-2 text-[15px] poppins-regular
+                className="flex size-full items-center justify-evenly gap-2 text-[16px] poppins-regular
                 max-lg:text-[1.3rem]
                 max-md:text-[1rem]
             ">
