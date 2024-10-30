3ca8a633b5e20dbd9bad5cbf220325f27807fb49
Mriganka Paul <ironmaniscool99@gmail.com>
2024-10-30 18:25:32 +0530

    removed loader

--- src/App.jsx
+++ src/App.jsx
@@ -1,38 +1,13 @@
-import { useEffect, useState } from "react";
 import GridConatainer from "./components/GridContainer";
 import Navbar from "./components/Navbar";
 // import AccordionPage from "./sections/AccordianPage";
 
 function App() {
-    const [loading, setLoading] = useState(true);
-
-    useEffect(() => {
-        // Event handler for load
-        const handleLoad = () => {
-            setTimeout(() => {
-                setLoading(false);
-              }, 2000);
-        };
-
-        // Add event listener for load
-        window.addEventListener('load', handleLoad);
-
-        // Cleanup function to remove event listener
-        return () => {
-            window.removeEventListener('load', handleLoad);
-        };
-    }, []);
-    return (<>
-    {loading ? (
-        <div>Loading</div>
-      ) : (
+    return (
         <div className="p-[15px] min-h-screen max-w-[1228px] my-0 mx-auto flex flex-col ">
-            <Navbar />
-            <GridConatainer />
+            <Navbar/>
+            <GridConatainer/>
         </div>
-      )}
-        
-        </>
     )
 }
 
