7c0411e65e99a5eed783f28d5b0de25abda63699
Mriganka Paul <ironmaniscool99@gmail.com>
2024-10-30 18:56:32 +0530

    test loader

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,37 @@
+import { useEffect, useState } from "react";
 import GridConatainer from "./components/GridContainer";
 import Navbar from "./components/Navbar";
 // import AccordionPage from "./sections/AccordianPage";
 
 function App() {
-    return (
-        <div className="p-[15px] min-h-screen max-w-[1228px] my-0 mx-auto flex flex-col ">
-            <Navbar/>
-            <GridConatainer/>
-        </div>
-    )
+    const [loading, setLoading] = useState(true);
+
+    useEffect(() => {
+        // Event handler for load
+        const handleLoad = () => {
+            setTimeout(() => {
+                setLoading(false);
+
+            }, 2000);
+        };
+
+        // Add event listener for load
+        window.addEventListener('load', handleLoad);
+
+        // Cleanup function to remove event listener
+        return () => {
+            window.removeEventListener('load', handleLoad);
+        };
+    }, []);
+
+
+    return (<>
+        {loading ? <div>loading...</div> :
+            <div className="p-[15px] min-h-screen max-w-[1228px] my-0 mx-auto flex flex-col ">
+                <Navbar />
+                <GridConatainer />
+            </div>}
+    </>)
 }
 
 export default App;
